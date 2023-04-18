99c80e020fefdcb67004ceb17f3e82ecbb2a03e1
Cresence <devharris93@gmail.com>
2023-04-18 18:45:05 -0400

    Complete state and trivia logic to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,42 @@ import {  fetchQuizQuestions } from './components/API';
 // Components
 import QuestionCard from './components/QuestionCard';
 // Types
-import { Difficulty } from './components/API';
+import { Difficulty, QuesionState } from './components/API';
 
 const App = () => {
+
+  type AnswerObject = {
+    question: string;
+    answer: string;
+    correct: boolean;
+    correctAnswer: string;
+  };
+  
   const TOTAL_QUESTIONS = 10;
 
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuesionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+  console.log(questions);
 
   const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false)
 
+    const newQuestions = await fetchQuizQuestions(
+      TOTAL_QUESTIONS,
+      Difficulty.EASY
+    );
+
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
 
   }
 
